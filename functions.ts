4d82ff84b89307e5c74b1374eaa2879d8f337009
Pasha1968 <42270001+Pasha1968@users.noreply.github.com>
2020-08-17 11:20:44 +0300

    Update functions.ts

--- functions.ts
+++ functions.ts
@@ -25,54 +25,80 @@ function selectseason(episodes:Episode[],season?:number){
 //     ])(season).length
 //   }
 export function countseries(episodes:Episode[],season?:number): number{
-    const tm = selectseason(episodes,season)
-    return tm.length
+    //const tm = selectseason(episodes,season)
+    return selectseason(episodes,season).length
 }
 export  function countruntime(episodes:Episode[],season?:number): number{
-    const tm = selectseason(episodes,season)
-    let tmp = 0
+   // let tmp = 0
     //let ar = R.map( R.lens(R.prop('runtime'),tm)
    // let ar = R.map(R.lensPath(['runtime']),tm)
-    const arr = R.map(R.prop('runtime'), tm)
-    tmp = R.sum(arr)
+    // const arr = R.map(R.prop('runtime'), selectseason(episodes,season))
+    // tmp = R.sum(arr)
    // console.log(R.reduce(R.add,tmp,tm[0].runtime))
     // for (var prop in tm) {
     //   tmp = tmp+tm[prop].runtime
     // }
    //R.forEach(tmp = tmp +tm.runtime,tm)
-    return tmp
-  } 
-  // function fr(accumulator:number,sum:number){
-  //   accumulator = accumulator+sum
-  // }
+    return R.sum(
+        R.map(
+            R.prop('runtime'),
+            selectseason(
+                episodes,
+                season
+            )
+        )
+    )
+}
 export function findmax(episodes:Episode[],season?:number): number{
-    let tm = selectseason(episodes,season)
-    let tmp = 0
-    const arr = R.map(R.prop('runtime'), tm)
-    tmp = R.reduce(R.max, -Infinity, arr)
-    //console.log(R.forEach(R.max(tmp,arr),arr))
-   //R.forEach(tmp = tmp +tm.runtime,tm)
-    return tmp
+   // const arr = R.map(R.prop('runtime'), selectseason(episodes,season))
+    return R.reduce(
+        R.max,
+        -Infinity,
+        R.map(
+            R.prop('runtime'),
+            selectseason(
+                episodes,
+                season
+            )
+        )
+    )
   }
 export function findmin(episodes:Episode[],season?:number): number{
-    let tm = selectseason(episodes,season)
-   let tmp //= tm[0].runtime
-    const arr = R.map(R.prop('runtime'), tm)
-    tmp = R.reduce(R.min, Infinity, arr)
-   //R.forEach(tmp = tmp +tm.runtime,tm)
-    return tmp
+//    const arr = R.map(R.prop('runtime'), selectseason(episodes,season))
+    return R.reduce(
+        R.min,
+        Infinity,
+        R.map(
+            R.prop('runtime'),
+            selectseason(episodes,season)
+        )
+    )
   }
 export function first(episodes:Episode[],season?:number): number{
-    let tm = selectseason(episodes,season)
-    let tmp //= tm[0].airstamp
-    const arr = R.map(R.prop('airstamp'), tm)
-    tmp = R.reduce(R.min, Infinity, arr)
-    return tmp
+    //const arr = R.map(R.prop('airstamp'), selectseason(episodes,season))
+    return R.reduce(
+        R.min,
+        Infinity,
+        R.map(
+            R.prop('airstamp'),
+            selectseason(
+                episodes,
+                season
+            )
+        )
+    )
   }
 export function last(episodes:Episode[],season?:number): number{
-    let tm = selectseason(episodes,season)
-    let tmp //= tm[0].airstamp
-    const arr = R.map(R.prop('airstamp'), tm)
-    tmp = R.reduce(R.max, -Infinity, arr)
-    return tmp
+    //const arr = R.map(R.prop('airstamp'), selectseason(episodes,season))
+    return R.reduce(
+        R.min,
+        Infinity,
+        R.map(
+            R.prop('airstamp'),
+            selectseason(
+                episodes,
+                season
+            )
+        )
+    )
   }
